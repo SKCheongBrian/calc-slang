522ae92a86aee7cbfcb5be6159549b7a8b666401
erisjacey-MACBOOK <eris_jacey@hotmail.com>
2023-03-07 13:44:48 +0800

    Better typechecker

--- src/typechecker/typechecker.ts
+++ src/typechecker/typechecker.ts
@@ -1,7 +1,8 @@
+import _ from 'lodash'
 import * as t from '../types'
 
 export class TypeChecker {
   static isEqual(left: t.Type | undefined, right: t.Type | undefined): boolean {
-    return typeof left === typeof right
+    return _.isEqual(left, right)
   }
 }
