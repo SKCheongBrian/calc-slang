0bd164e13e5c227e01a1a6239be1e77d908b723e
skcheongbrian <skcheongbrian@gmail.com>
2023-03-19 17:24:35 +0800

    temp

--- src/interpreter/Memory.ts
+++ src/interpreter/Memory.ts
@@ -1,21 +1,21 @@
 export default class Memory {
-    public memory: DataView
-    private MEGA: number = 2 ** 20
-    private WORD_SIZE: number = 8
+  public memory: DataView
+  private MEGA: number = 2 ** 20
+  private WORD_SIZE: number = 8
 
-    private free: number
+  private free: number
 
-    public constructor(size: number) {
-        const data = new ArrayBuffer(size * this.MEGA)
-        this.memory = new DataView(data)
-        this.free = 0
-    }
+  public constructor(size: number) {
+    const data = new ArrayBuffer(size * this.MEGA)
+    this.memory = new DataView(data)
+    this.free = 0
+  }
 
-    public get_word_at_index(index: number) {
-        this.memory.getFloat64(index * this.WORD_SIZE)
-    }
+  public get_word_at_index(index: number) {
+    this.memory.getFloat64(index * this.WORD_SIZE)
+  }
 
-    public set_word_at_index(index: number, value: number) {
-        this.memory.setFloat64(index * this.WORD_SIZE, value)
-    }
+  public set_word_at_index(index: number, value: number) {
+    this.memory.setFloat64(index * this.WORD_SIZE, value)
+  }
 }
