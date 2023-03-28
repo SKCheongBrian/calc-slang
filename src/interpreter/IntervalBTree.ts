3ef3669325a56606a1b922f677d8f116bb7d075f
ruoyann <ruoyan@u.nus.edu>
2023-03-28 17:02:28 +0800

    formatting

--- src/interpreter/IntervalBTree.ts
+++ src/interpreter/IntervalBTree.ts
@@ -2,208 +2,209 @@ import Interval from './interval'
 import Node from './node'
 
 export default class AVLTree {
-  private root: Node | null 
+  private root: Node | null
 
   public constructor() {
     this.root = null
   }
-  
+
   //return height of the node
-  public get_height(N: Node | null) : number {
-    if (N === null){
-      return 0;
+  public get_height(N: Node | null): number {
+    if (N === null) {
+      return 0
     }
-    return N.get_height();
+    return N.get_height()
   }
-  
+
   //right rotate
   public rightRotate(y: Node): Node | null {
-    const x : Node | null = y.get_left();
+    const x: Node | null = y.get_left()
     let T2 = null
     if (!x) {
       return null
     }
-    T2 = x.get_right() 
-    x.set_right(y) 
+    T2 = x.get_right()
+    x.set_right(y)
     y.set_left(T2)
     y.set_height(Math.max(this.get_height(y.get_left()), this.get_height(y.get_right())) + 1)
     x.set_height(Math.max(this.get_height(x.get_left()), this.get_height(x.get_right())) + 1)
-    return x;
+    return x
   }
 
   //right rotate
   public leftRotate(x: Node): Node | null {
-    const y : Node | null = x.get_right();
+    const y: Node | null = x.get_right()
     if (!y) {
       return null
     }
-    const T2 = y.get_left() 
-    y.set_left(x) 
+    const T2 = y.get_left()
+    y.set_left(x)
     x.set_right(T2)
     x.set_height(Math.max(this.get_height(x.get_left()), this.get_height(x.get_right())) + 1)
     y.set_height(Math.max(this.get_height(y.get_left()), this.get_height(y.get_right())) + 1)
-    return y;
+    return y
   }
-  
+
   // get balance factor of a node
-  public getBalanceFactor(N: Node) : number{
-    if (N == null){
-      return 0;
+  public getBalanceFactor(N: Node): number {
+    if (N == null) {
+      return 0
     }
-    return this.get_height(N.get_left()) - this.get_height(N.get_right());
+    return this.get_height(N.get_left()) - this.get_height(N.get_right())
   }
-  
-  
+
   // helper function to insert a node
   public insertNodeHelper(node: Node | null, item: Interval) {
-
     // find the position and insert the node
-    if (node === null){
-      return (new Node(item));
+    if (node === null) {
+      return new Node(item)
     }
-    
-    if (item < node.get_item()){
+
+    if (item < node.get_item()) {
       node.set_left(this.insertNodeHelper(node.get_left(), item))
-    }else if (item > node.get_item()){
+    } else if (item > node.get_item()) {
       node.set_right(this.insertNodeHelper(node.get_right(), item))
-    }else{
-      return node;
+    } else {
+      return node
     }
-    
+
     // update the balance factor of each node
     // and, balance the tree
-    node.set_height(1 + Math.max(this.get_height(node.get_left()), this.get_height(node.get_right())))
-    
-    let balanceFactor = this.getBalanceFactor(node);
-    
+    node.set_height(
+      1 + Math.max(this.get_height(node.get_left()), this.get_height(node.get_right()))
+    )
+
+    const balanceFactor = this.getBalanceFactor(node)
+
     if (balanceFactor > 1) {
       const node_left = node.get_left()
       if (node_left) {
         if (item < node_left.get_item()) {
-          return this.rightRotate(node);
+          return this.rightRotate(node)
         } else if (item > node_left.get_item()) {
           node.set_left(this.leftRotate(node_left))
-          return this.rightRotate(node);
+          return this.rightRotate(node)
         }
       }
     }
-    
+
     if (balanceFactor < -1) {
       const node_right = node.get_right()
       if (node_right) {
         if (item > node_right.get_item()) {
-          return this.leftRotate(node);
+          return this.leftRotate(node)
         } else if (item < node_right.get_item()) {
           node.set_right(this.rightRotate(node_right))
-          return this.leftRotate(node);
+          return this.leftRotate(node)
         }
       }
     }
-    return node;
+    return node
   }
-  
+
   // insert a node
-  public insertNode(item : Interval) {
+  public insertNode(item: Interval) {
     // console.log(root);
-    this.root = this.insertNodeHelper(this.root, item);
+    this.root = this.insertNodeHelper(this.root, item)
   }
-  
+
   //get node with minimum value
-  public nodeWithMimumValue (node: Node | null) {
+  public nodeWithMimumValue(node: Node | null) {
     if (node == null) {
       return null
     }
-    let current = node;
+    let current = node
     let curr_left = current.get_left()
-    while (curr_left !== null){
-      current = curr_left;
+    while (curr_left !== null) {
+      current = curr_left
       curr_left = current.get_left()
     }
-    return current;
+    return current
   }
-  
-  // delete helper
-  public deleteNodeHelper(root: Node | null, item: Interval)  {
 
+  // delete helper
+  public deleteNodeHelper(root: Node | null, item: Interval) {
     // find the node to be deleted and remove it
-    if (root == null){
-      return root;
+    if (root == null) {
+      return root
     }
-    if (item < root.get_item()){
+    if (item < root.get_item()) {
       root.set_left(this.deleteNodeHelper(root.get_left(), item))
-    }else if (item > root.get_item()){
+    } else if (item > root.get_item()) {
       root.set_right(this.deleteNodeHelper(root.get_right(), item))
-    }else {
-      if ((root.get_left() === null) || (root.get_right() === null)) {
-        let temp = null;
-        if (temp == root.get_left()){
-          temp = root.get_right();
-        }else{
-          temp = root.get_left();
+    } else {
+      if (root.get_left() === null || root.get_right() === null) {
+        let temp = null
+        if (temp == root.get_left()) {
+          temp = root.get_right()
+        } else {
+          temp = root.get_left()
         }
-        
+
         if (temp == null) {
-          temp = root;
-          root = null;
-        } else{
-          root = temp;
+          temp = root
+          root = null
+        } else {
+          root = temp
         }
       } else {
-        let temp = this.nodeWithMimumValue(root.get_right());
+        const temp = this.nodeWithMimumValue(root.get_right())
         if (temp) {
           root.set_item(temp.get_item())
           root.set_right(this.deleteNodeHelper(root.get_right(), temp.get_item()))
         }
       }
     }
-    if (root == null){
-      return root;
+    if (root == null) {
+      return root
     }
 
     // Update the balance factor of each node and balance the tree
-    root.set_height(Math.max(this.get_height(root.get_left()), this.get_height(root.get_right())) + 1)
-    
-    let balanceFactor = this.getBalanceFactor(root);
+    root.set_height(
+      Math.max(this.get_height(root.get_left()), this.get_height(root.get_right())) + 1
+    )
+
+    const balanceFactor = this.getBalanceFactor(root)
     if (balanceFactor > 1) {
       const root_left = root.get_left()
       if (root_left) {
         if (this.getBalanceFactor(root_left) >= 0) {
-          return this.rightRotate(root);
+          return this.rightRotate(root)
         } else {
           root.set_left(this.leftRotate(root_left))
-          return this.rightRotate(root);
+          return this.rightRotate(root)
         }
       }
     }
     if (balanceFactor < -1) {
       const root_right = root.get_right()
       if (root_right) {
-      if (this.getBalanceFactor(root_right) <= 0) {
-        return this.leftRotate(root);
-      } else {
-        root.set_right(this.rightRotate(root_right))
-        return this.leftRotate(root);
-      }
+        if (this.getBalanceFactor(root_right) <= 0) {
+          return this.leftRotate(root)
+        } else {
+          root.set_right(this.rightRotate(root_right))
+          return this.leftRotate(root)
+        }
       }
     }
-    return root;
+    return root
   }
-  
+
   //delete a node
-  public deleteNode (item: Interval) {
-    this.root = this.deleteNodeHelper(this.root, item);
+  public deleteNode(item: Interval) {
+    this.root = this.deleteNodeHelper(this.root, item)
   }
-  
+
   // print the tree in pre - order
   public preOrder = () => {
-    this.preOrderHelper(this.root);
+    this.preOrderHelper(this.root)
   }
-  
+
   public preOrderHelper = (node: Node | null) => {
     if (node) {
-      console.log(node.get_item());
-      this.preOrderHelper(node.get_left());
-      this.preOrderHelper(node.get_right());
+      console.log(node.get_item())
+      this.preOrderHelper(node.get_left())
+      this.preOrderHelper(node.get_right())
     }
   }
 }
